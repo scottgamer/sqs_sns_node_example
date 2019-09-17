ae6ff22183a3fbd04f88508aed268c7557147a1e
Richard Munoz <richosojason@msn.com>
2019-09-17 14:22:41 -0500

    added routes

--- src/app.js
+++ src/app.js
@@ -1,6 +1,12 @@
 import express from 'express';
 
-const app = express();
+import snsRoutes from './http/routes/snsRoutes';
+import sqsRoutes from './http/routes/sqsRoutes';
+
 const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.use('/api/notification', snsRoutes);
+app.use('/api/queue', sqsRoutes);
 
 app.listen(PORT, () => console.log('Listening on port', PORT));
---
+++ src/http/routes/sqsRoutes.js
@@ -0,0 +1,8 @@
+import { Router } from 'express';
+const router = Router();
+
+router.get('/hello', (req, res) => {
+  res.status(200).send('Hello');
+});
+
+export default router;
